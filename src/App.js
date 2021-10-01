4ece508be67508446f7e4c7b8fc74af3a976f4e7
harisliv <livieratos.theoxaris@gmail.com>
2021-10-01 18:02:29 +0300

    form and submit handler

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { useState } from 'react/cjs/react.development';
 import './App.css';
+import Form from './Form';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const formSubmitHandler = (input) => {
+    setTodos(prev => prev.concat({
+      id: Date.now(),
+      content: input,
+      completed: false
+    }))
+  }
+
+  console.log(todos);
+
   return (
     <div className="App">
-
+      <Form formSubmit={formSubmitHandler}/>
     </div>
   );
 }
---
+++ src/Form.js
@@ -0,0 +1,30 @@
+import React, {useState} from "react";
+
+const Form = ({formSubmit}) => {
+    const [input, setInput] = useState('');
+
+    const changeHandler = (event) => {
+        setInput(event.target.value);
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        // console.log(input);
+        formSubmit(input);
+        setInput('');
+    }
+
+  return (
+    <form onSubmit={submitHandler}>
+      <input
+        type="text"
+        name="main_input"
+        value={input}
+        onChange={changeHandler}
+      />
+      <button type="submit">Submit!</button>
+    </form>
+  );
+};
+
+export default Form;
